27c389b713f79f2a628d2d61d9a2b6915e7260b3
Pavani <pavani.software26@gmail.com>
2026-02-21 10:37:09 -0600

    coversastion completed

--- cmd-variables.sh
+++ cmd-variables.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
-START_TIME=$(date)
+START_TIME=$(date +%s)
 echo "script excetured at $START_TIME"
 sleep 10
-END_TIME=$(date)
+END_TIME=$(date +%s)
 echo "script ended at $END_TIME"
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "script executed in $TOTAL_TIME)
