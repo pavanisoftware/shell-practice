bfe956cca78bd8814419a44ebb6c6ee618c524f7
Pavani <pavani.software26@gmail.com>
2026-02-21 10:05:16 -0600

    coversastion completed

--- coversastion.sh
+++ coversastion.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-echo "pav:hello"
-echo "kir:hi"
-echo "pav:how r u"
-echo "kir:gud, how about u pav"
+PERSON1=pav
+PERSON2=kir
+ echo "$PERSON1:hello"
+echo "$PERSON2:hi"
+echo "$PERSON1:how r u"
+echo "$PERSON2:gud, how about u $PERSON1"
