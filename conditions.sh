8e07134b2fea6ba3f7c78a2962a2d3706e0a340b
Pavani <pavani.software26@gmail.com>
2026-02-21 11:27:44 -0600

    coversastion completed

--- conditions.sh
+++ conditions.sh
@@ -2,6 +2,8 @@
 NUMBER=$1
 if [ $NUMBER -gt 20 ]; then
     echo " given number is $NUMBER is grester than 20"
+elif [ $NUMBER -eq 20]; then 
+    echo "Giuven numbwer $NUMBER is equal to 20"
 else
     echo "Given  umber $NUMBER  is less than 20 "
 fi
